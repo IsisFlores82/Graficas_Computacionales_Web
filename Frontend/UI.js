7c2cbc8d77db549f67b646716f68ea2ca2794791
Nevi63 <cdpinkus@gmail.com>
2024-11-25 02:45:18 -0600

    Pausa
    picas esc y sale el menu de pausa

--- Frontend/UI.js
+++ Frontend/UI.js
@@ -13,5 +13,26 @@ const pauseModalElement = document.getElementById('PauseModal');
  });
 
  document.getElementById('BtnSalir').addEventListener('click', function() {
-    window.location.href = 'index.html';
+    window.location.href = '/';
 });
+
+document.getElementById('BtnContinuar').addEventListener('click', function() {
+   pauseModal.hide();
+});
+
+function obtenerNombreJugador() {
+    // Obtener el nombre del jugador desde localStorage
+    const jugadorNombre = localStorage.getItem('playerName');
+    
+    // Verificar si el nombre está disponible
+    if (jugadorNombre) {
+      // Actualizar el contenido del elemento con id="cronometro"
+      const namePlayerElement = document.getElementById('namePlayer');
+      namePlayerElement.textContent = jugadorNombre;
+    } else {
+      console.error('No se ha encontrado el nombre del jugador en localStorage');
+    }
+  }
+  
+  // Llamar a la función para obtener el nombre cuando la página se carga
+  window.onload = obtenerNombreJugador;
